0eb7034cadf630d6030dd1f6c4b26c1bd2129e77
Giorgos Desktop <plokamistr@gmail.com>
2021-08-30 15:59:38 +0200

    fixes

--- src/js/functions.js
+++ src/js/functions.js
@@ -3,17 +3,17 @@ document.title = `Hubspot Multistep Form at ${window.location.hostname}`;
 
 // Adds numbered id to each section
 $("#hs-main-form section").each(function (i) {
-  i = i + 1;
-  $(this).prop('id', 'step-' + i);
+    i = i + 1;
+    $(this).prop('id', 'step-' + i);
 });
 
 // Dynamic width of progress bar steps
 $("#hs-main-form #progressbar").each(function () {
-  let liNumber = $(this).children().length;
-  let liWidth = 100 / liNumber;
-  $(this)
-    .find("li")
-    .css("width", liWidth + "%");
+    let liNumber = $(this).children().length;
+    let liWidth = 100 / liNumber;
+    $(this)
+        .find("li")
+        .css("width", liWidth + "%");
 });
 
 // Get IP of Client-User
@@ -21,10 +21,10 @@ let ipAddress = [];
 const urlIp = "https://api64.ipify.org?format=json";
 
 fetch(urlIp)
-  .then((response) => response.json())
-  .then((result) => {
-    ipAddress.push(result.ip);
-  });
+    .then((response) => response.json())
+    .then((result) => {
+        ipAddress.push(result.ip);
+    });
 
 // Local Storage Service from hubspotscripts
 /*
@@ -44,131 +44,130 @@ function setLocalStorageService(fields) {
 
 // Autocomplete state n city function
 $("#hs-main-form input[name='zip']").on("keyup", function () {
-  const zipValue = $(this).val();
-  const zipUrl = `https://api.zippopotam.us/us/${form.querySelector("input[name='zip']").value}`
-  // We execute when length is 5 digits
-  if ((zipValue.length == 5)) {
+    const zipValue = $(this).val();
+    const zipUrl = `https://api.zippopotam.us/us/${form.querySelector("input[name='zip']").value}`
+    // We execute when length is 5 digits
+    if ((zipValue.length == 5)) {
 
-    async function fetchZip() {
+        async function fetchZip() {
 
-      const response = await fetch(zipUrl);
-      if (response.status === 200) {
-        const data = await response.json();
+            const response = await fetch(zipUrl);
+            if (response.status === 200) {
+                const data = await response.json();
 
-        places = data['places'][0];
-        $("input[name='city']").val(places['place name']);
-        $("select[name='state']").val(places['state']);
-        console.log(`WE GET : Zip Code: ${data['post code']} | City: ${places['place name']} | State: ${places['state']} `);
-      } else {
-        alert("Postal Code Not Found. Please try again.");
-      }
+                places = data['places'][0];
+                $("input[name='city']").val(places['place name']);
+                $("select[name='state']").val(places['state']);
+                console.log(`WE GET : Zip Code: ${data['post code']} | City: ${places['place name']} | State: ${places['state']} `);
+            } else {
+                alert("Postal Code Not Found. Please try again.");
+            }
+        }
+        fetchZip().then(data => {
+            data; // fetched data
+        });
     }
-    fetchZip().then(data => {
-      data; // fetched data
-    });
-  }
 });
 
 
 // Form Validation
 function formValidation() {
 
-  // Email validation
-  $.validator.addMethod("customEmail", function (value, element) {
-    return this.optional(element) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
-  }, "Please enter valid email address!");
+    // Email validation
+    $.validator.addMethod("customEmail", function (value, element) {
+        return this.optional(element) || /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(value);
+    }, "Please enter valid email address!");
 
-  $.validator.addMethod("noSpace", function (value, element) {
-    return value == '' || value.trim().length != 0;
-  }, "Please fill the field correctly. Avoid 'space' characters");
+    $.validator.addMethod("noSpace", function (value, element) {
+        return value == '' || value.trim().length != 0;
+    }, "Please fill the field correctly. Avoid 'space' characters");
 
 
-  // Add class ignore to active section
-  $(form).validate({
+    // Add class ignore to active section
+    $(form).validate({
 
-    rules: {
-      zip: {
-        required: true,
-        noSpace: true,
-      },
-      roof_stories: {
-        required: true,
-      },
-      approximate_roof_size: {
-        required: true,
-      },
-      firstname: {
-        required: true,
-        noSpace: true,
-      },
-      lastname: {
-        required: true,
-        noSpace: true,
-      },
-      email: {
-        required: true,
-        customEmail: true,
-        noSpace: true,
-      },
-      phone: {
-        required: true,
-        noSpace: true,
-      },
-      address: {
-        required: true,
-        noSpace: true,
-      },
-      city: {
-        required: true,
-        noSpace: true,
-      },
-    },
-    messages: {
-      zip: {
-        required: 'A zip code is required!',
-      },
-      roof_stories: {
-        required: 'Please choose Level/s'
-      },
-      approximate_roof_size: {
-        required: 'Please choose a Range'
-      },
-      email: {
-        required: 'Please enter a valid email',
-        email: 'Don\'t forget the @ character!',
-      },
-      firstname: {
-        required: 'Please enter your first name',
-      },
-      lastname: {
-        required: 'Please enter your last name',
-      },
-      phone: {
-        required: 'Please enter a valid phone number',
-      },
-      address: {
-        required: 'Please enter a valid address',
-      },
-      city: {
-        required: 'Please enter a valid city name',
-      },
-    },
-    ignore: ".ignore",
-    focusInvalid: true,
+        rules: {
+            zip: {
+                required: true,
+                noSpace: true,
+            },
+            roof_stories: {
+                required: true,
+            },
+            approximate_roof_size: {
+                required: true,
+            },
+            firstname: {
+                required: true,
+                noSpace: true,
+            },
+            lastname: {
+                required: true,
+                noSpace: true,
+            },
+            email: {
+                required: true,
+                customEmail: true,
+                noSpace: true,
+            },
+            phone: {
+                required: true,
+                noSpace: true,
+            },
+            address: {
+                required: true,
+                noSpace: true,
+            },
+            city: {
+                required: true,
+                noSpace: true,
+            },
+        },
+        messages: {
+            zip: {
+                required: 'A zip code is required!',
+            },
+            roof_stories: {
+                required: 'Please choose Level/s'
+            },
+            approximate_roof_size: {
+                required: 'Please choose a Range'
+            },
+            email: {
+                required: 'Please enter a valid email',
+                email: 'Don\'t forget the @ character!',
+            },
+            firstname: {
+                required: 'Please enter your first name',
+            },
+            lastname: {
+                required: 'Please enter your last name',
+            },
+            phone: {
+                required: 'Please enter a valid phone number',
+            },
+            address: {
+                required: 'Please enter a valid address',
+            },
+            city: {
+                required: 'Please enter a valid city name',
+            },
+        },
+        ignore: ".ignore",
+        focusInvalid: true,
 
-    errorPlacement: function (error, element) {
-      if (element.is(":radio")) {
-        error.insertBefore($(element).parent().parent());
-      }
-      // Uncomment if other type of element
-      /* 
-      else if(element.is( ":text" )){
-        error.insertBefore($(element));
-      }*/
-      else {
-        error.insertBefore($(element));
-      }
-    },
-  });
+        errorPlacement: function (error, element) {
+            if (element.is(":radio")) {
+                error.insertBefore($(element).parent().parent());
+            }
+            // Uncomment if other type of element
+            /* 
+            else if(element.is( ":text" )){
+              error.insertBefore($(element));
+            }*/
+            else {
+                error.insertBefore($(element));
+            }
+        },
+    });
 }
-
--- src/js/multistep.js
+++ src/js/multistep.js
@@ -1,7 +1,6 @@
 let currentStep, nextStep, prevStep; //sections
 let left, opacity, scale; //section properties which we will animate
 let animating; //flag to prevent quick multi-click glitches
-let clicks = 0; // iniciate clicks on next button
 let afterEmail = ""; // iniciate flag to start submiting after the step that includes the email field
 const nextBtn = $(".next");
 const prevBtn = $(".previous");
@@ -10,94 +9,90 @@ const prevBtn = $(".previous");
 //################## ON NEXT CLICK #########################
 $(nextBtn).on("click", async function () {
 
-  //debugger
-  formValidation()
+    //debugger
+    formValidation()
 
-  if ($(form).valid() === true) {
+    if ($(form).valid() === true) {
 
-    //increase next button count and assign the steps completed value to steps_completed
-    clicks++;
-    form.querySelector("input[name='steps_completed']").value = clicks;
+        //increase next button count and assign the steps completed value to steps_completed
+        let numberStepsCompleted = $('#progressbar li.active').length
+        form.querySelector("input[name='steps_completed']").value = numberStepsCompleted;
+        console.log(numberStepsCompleted);
 
 
-    if (animating) return false;
+        if (animating) return false;
 
-    currentStep = $(this).parent().parent();
-    nextStep = $(this).parent().parent().next();
-    //console.log(nextStep);
+        currentStep = $(this).parent().parent();
+        nextStep = $(this).parent().parent().next();
+        //console.log(nextStep);
 
-    // HERE WE SUBMIT ON THE STEP WHERE THE EMAIL INPUT IS
-    let emailEl = form.querySelector("input[name='email']");
-    let divContains_El = form.querySelector("section.activeStep").contains(emailEl);
+        // HERE WE SUBMIT ON THE STEP WHERE THE EMAIL INPUT IS
+        let emailEl = form.querySelector("input[name='email']");
+        let divContains_El = form.querySelector("section.activeStep").contains(emailEl);
 
-    if (divContains_El || afterEmail) {
-      console.log("YES");
-      const submitValid = await submitForm();
+        if (divContains_El || afterEmail) {
+            console.log("YES");
+            const submitValid = await submitForm();
 
-      //Comment for testing NO SUMBISSION
-      // We check if submit on Hubspot returns true of false
+            //Comment for testing NO SUMBISSION
+            // We check if submit on Hubspot returns true of false
+            if (!submitValid) {
+                return
+            }
+            //Comment for testing NO SUMBISSION
 
-      if (!submitValid) {
-        return
-      }
+            afterEmail = true;
+        } else {
+            console.log("NO");
+        };
 
-      //Comment for testing NO SUMBISSION
+        // To validate only the indicated fields if not remove class ignore when activeStep
+        $(nextStep).find(".ignore").removeClass();
+        animating = true;
 
-      afterEmail = true;
+        //activate next step on progressbar using the index of nextStep
+        $("#progressbar li").eq($("section").index(nextStep)).addClass("active");
+        $(currentStep).removeClass("activeStep");
+        $(nextStep).addClass("activeStep");
+
+        // HERE WE PASS clicks TO USER INFO REGISTRATION
+        //clickRegister(clicks);
+
+        //show the next section
+        nextStep.css("visibility", "visible");
+        //hide the current section with style
+        currentStep.animate({ opacity: 0 }, {
+            step: function (now, mx) {
+                //as the opacity of currentStep reduces to 0 - stored in "now"
+                //1. scale currentStep down to 80%
+                scale = 1 - (1 - now) * 0.2;
+                //2. bring nextStep from the right(50%)
+                left = now * 50 + "%";
+                //3. increase opacity of nextStep to 1 as it moves in
+                opacity = 1 - now;
+                currentStep.css({ transform: "scale(" + scale + ")" });
+                nextStep.css({ left: left, opacity: opacity });
+            },
+            duration: 800,
+            complete: function () {
+                currentStep.css("visibility", "hidden");
+                animating = false;
+            },
+            //this comes from the custom easing plugin
+            easing: "easeInOutBack",
+        });
+        // Animate progress Bar  
+        $(".progressbar-container").css("opacity", "0");
+        setTimeout(function () {
+            $(".progressbar-container").animate({
+                opacity: 1
+            }, 500, "swing");
+        }, 300);
+        // Animate progress Bar
+
     } else {
-      console.log("NO");
-    };
-
-    // To validate only the indicated fields if not remove class ignore when activeStep
-    $(nextStep).find(".ignore").removeClass();
-    animating = true;
-
-    //activate next step on progressbar using the index of nextStep
-    $("#progressbar li").eq($("section").index(nextStep)).addClass("active");
-    $(currentStep).removeClass("activeStep");
-    $(nextStep).addClass("activeStep");
-
-    // HERE WE PASS clicks TO USER INFO REGISTRATION
-    //clickRegister(clicks);
-
-    //show the next section
-    nextStep.css("visibility", "visible");
-    //hide the current section with style
-    currentStep.animate(
-      { opacity: 0 },
-      {
-        step: function (now, mx) {
-          //as the opacity of currentStep reduces to 0 - stored in "now"
-          //1. scale currentStep down to 80%
-          scale = 1 - (1 - now) * 0.2;
-          //2. bring nextStep from the right(50%)
-          left = now * 50 + "%";
-          //3. increase opacity of nextStep to 1 as it moves in
-          opacity = 1 - now;
-          currentStep.css({ transform: "scale(" + scale + ")" });
-          nextStep.css({ left: left, opacity: opacity });
-        },
-        duration: 800,
-        complete: function () {
-          currentStep.css("visibility", "hidden");
-          animating = false;
-        },
-        //this comes from the custom easing plugin
-        easing: "easeInOutBack",
-      }
-    );
-    // Animate progress Bar  
-    $(".progressbar-container").css("opacity", "0");
-    setTimeout(function () {
-      $(".progressbar-container").animate({
-        opacity: 1
-      }, 500, "swing");
-    }, 300);
-    // Animate progress Bar
-
-  } else {
-    return;
-  }
+        return;
+    }
 });
 //####################################################################
 
@@ -215,124 +210,118 @@ $(resetBtn).on("click", function () {
 
 
 //################## IF AUTO RESET IS WANTED #########################
-
+/*
 function resetForm() {
-  $(form)[0].reset();
-  clicks = 0;
-  afterEmail = false;
-  $(".valid").toggleClass("ignore");
-  $("#step-1 .fields-section input").removeClass("ignore");
-  $(".checkbox-field").removeClass("selected");
+    $(form)[0].reset();
+    clicks = 0;
+    afterEmail = false;
+    $(".valid").toggleClass("ignore");
+    $("#step-1 .fields-section input").removeClass("ignore");
+    $(".checkbox-field").removeClass("selected");
 }
 
 function returnToFirst() {
-  if (animating) return false;
-  animating = true;
+    if (animating) return false;
+    animating = true;
 
-  currentStep = $("section.activeStep");
-  firstStep = $("section").first();
-  allSteps = $("section");
+    currentStep = $("section.activeStep");
+    firstStep = $("section").first();
+    allSteps = $("section");
 
-  //de-activate current step and set active class to the first step on progressbar
-  $("#progressbar li").removeClass("active");
-  $("#progressbar li").first().addClass("active");
+    //de-activate current step and set active class to the first step on progressbar
+    $("#progressbar li").removeClass("active");
+    $("#progressbar li").first().addClass("active");
 
-  //de-activate ActiveStep on currentStep and set active class to the firstStep
-  $(currentStep).removeClass("activeStep");
-  $(firstStep).addClass("activeStep");
+    //de-activate ActiveStep on currentStep and set active class to the firstStep
+    $(currentStep).removeClass("activeStep");
+    $(firstStep).addClass("activeStep");
 
-  // show the first section
-  $(firstStep).css({
-    visibility: "visible",
-  });
+    // show the first section
+    $(firstStep).css({
+        visibility: "visible",
+    });
 
-  // Reset All step to original size
-  $(allSteps).css({ transform: "scale(" + 1 + ")" });
+    // Reset All step to original size
+    $(allSteps).css({ transform: "scale(" + 1 + ")" });
 
-  //hide the current section with style
-  currentStep.animate(
-    { opacity: 0 },
-    {
-      step: function (now, mx) {
-        //as the opacity of currentStep reduces to 0 - stored in "now"
-        //1. scale previousStep from 80% to 100%
-        scale = 0.8 + (1 - now) * 0.2;
-        //2. take currentStep to the right(50%) - from 0%
-        left = (1 - now) * 50 + "%";
-        //3. increase opacity of previousStep to 1 as it moves in
-        opacity = 1 - now;
-        currentStep.css({ left: left });
-        firstStep.css({
-          transform: "scale(" + scale + ")",
-          opacity: opacity,
-        });
-      },
-      duration: 600,
-      complete: function () {
-        currentStep.css("visibility", "hidden");
-        animating = false;
-      },
-      //this comes from the custom easing plugin
-      easing: "easeOutBack",
-    }
-  );
+    //hide the current section with style
+    currentStep.animate({ opacity: 0 }, {
+        step: function(now, mx) {
+            //as the opacity of currentStep reduces to 0 - stored in "now"
+            //1. scale previousStep from 80% to 100%
+            scale = 0.8 + (1 - now) * 0.2;
+            //2. take currentStep to the right(50%) - from 0%
+            left = (1 - now) * 50 + "%";
+            //3. increase opacity of previousStep to 1 as it moves in
+            opacity = 1 - now;
+            currentStep.css({ left: left });
+            firstStep.css({
+                transform: "scale(" + scale + ")",
+                opacity: opacity,
+            });
+        },
+        duration: 600,
+        complete: function() {
+            currentStep.css("visibility", "hidden");
+            animating = false;
+        },
+        //this comes from the custom easing plugin
+        easing: "easeOutBack",
+    });
 }
 
-let observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    if (mutation.attributeName === "class") {
-      if ($(mutation.target).hasClass("activeStep")) {
+let observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.attributeName === "class") {
+            if ($(mutation.target).hasClass("activeStep")) {
 
-        // CountDown Timer to reset elements
-        let timeToReset = 6;
-        let downTimer = setInterval(function () {
-          if (timeToReset <= 0) {
-            clearInterval(downTimer);
-            resetForm();
-            returnToFirst();
-          }
-          document.getElementById("countProgressBar").value = 6 - timeToReset;
-          document.querySelector("#timeRem p").innerHTML =
-            timeToReset + " seconds to reset the form";
-          timeToReset -= 1;
-        }, 1000);
-      }
-    }
-  });
-  document.getElementById("countProgressBar").value = 0;
+                // CountDown Timer to reset elements
+                let timeToReset = 6;
+                let downTimer = setInterval(function() {
+                    if (timeToReset <= 0) {
+                        clearInterval(downTimer);
+                        resetForm();
+                        returnToFirst();
+                    }
+                    document.getElementById("countProgressBar").value = 6 - timeToReset;
+                    document.querySelector("#timeRem p").innerHTML =
+                        timeToReset + " seconds to reset the form";
+                    timeToReset -= 1;
+                }, 1000);
+            }
+        }
+    });
+    document.getElementById("countProgressBar").value = 0;
 });
 observer.observe(document.querySelector(".step-msg"), {
-  attributes: true,
-});
-
-//################## IF PAGE REDIRECTION IS WANTED #########################
-/*
-// We check if the last element of the multistep-form-wrapper has class activeStep and when he gets we reset or redirect
-let observer = new MutationObserver(function (mutations) {
-
-  mutations.forEach(function (mutation) {
-    if (mutation.attributeName === "class") {
-      if ($(mutation.target).hasClass("activeStep")) {
-        // CountDown Timer to redirect
-        let timeToReset = 6;
-        let downTimer = setInterval(function () {
-          if (timeToReset <= 0) {
-            clearInterval(downTimer);
-            window.open(redirUrl, '_self');
-          }
-          document.getElementById("countProgressBar").value = 6 - timeToReset;
-          document.querySelector("#timeRem p").innerHTML = timeToReset + " seconds to redirect you";
-          timeToReset -= 1;
-        }, 1000);
-      }
-    }
-  });
-  document.getElementById("countProgressBar").value = 0;
-});
-
-observer.observe(document.querySelector(".step-msg"), {
-  attributes: true,
+    attributes: true,
 });
 */
 
+//################## IF PAGE REDIRECTION IS WANTED #########################
+// We check if the last element of the multistep-form-wrapper has class activeStep and when he gets we reset or redirect
+let observer = new MutationObserver(function (mutations) {
 
+    mutations.forEach(function (mutation) {
+        if (mutation.attributeName === "class") {
+            if ($(mutation.target).hasClass("activeStep")) {
+                // CountDown Timer to redirect
+                let timeToReset = 2;
+                let downTimer = setInterval(function () {
+                    if (timeToReset <= 0) {
+                        clearInterval(downTimer);
+                        window.open(redirUrl, '_self');
+                    }
+                    document.getElementById("countProgressBar").value = 2 - timeToReset;
+                    document.querySelector("#timeRem p").innerHTML = timeToReset + " seconds to redirect you";
+                    timeToReset -= 1;
+                }, 1000);
+            }
+        }
+    });
+    document.getElementById("countProgressBar").value = 0;
+});
+
+observer.observe(document.querySelector(".step-msg"), {
+    attributes: true,
+});
